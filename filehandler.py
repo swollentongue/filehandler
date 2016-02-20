53de6541306a4a0fd5bc282fd2ce3b9ac87916d1
Rob Lee <robert.lee@iacpublishinglabs.com>
2016-02-19 16:24:48 -0800

    addition of base file handling classes.

--- filehandler.py
+++ filehandler.py
@@ -1,31 +1,84 @@
 import os
 import csv
-import 
+import openpyxl
+from argparse import ArgumentParser
+import pandas as pd
 
 class FileHandlerError(Exception):
     def __init__(self, value):
         self.value = value
-	def __str__(self):
-		return repr(self.value)
+    def __str__(self):
+        return repr(self.value)
 
-class FileHandler(class):
-	'''
-	class to handle multiple file types
-	usage f = FileHandler('/path/to/file/filename')
-	'''
+class UniversalFile(object):
+    '''
+    info tk
+    '''
 
-	self.extension_list = ('xls', 'xlsx', 'txt', 'csv')
+    extension_list = ('xls', 'xlsx', 'txt', 'csv')
 
-	def __init__(self, filename):
-		self.filename = filename
-		filename_path = os.path.split(filename)[0]
-		if filename_path != '':
-			self.path = filename_path
-		else:
-			self.path = os.getcwd()
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __iter__(self):
+        return self
+
+class TxtHandler(UniversalFile):
+    def __init__(self, filename):
+        UniversalFile.__init__(self, filename)
+        try:
+            self.base_file = open(filename, 'rU')
+        except IOError:
+            raise FileHandlerError('File error. Please make sure you types the correct filename.')
+
+    def next(self):
+        try:
+            line = self.base_file.next()
+            return line.strip().split('\t')
+        except StopIteration:
+            self.base_file.seek(0)
+            raise StopIteration
+
+class CSVHandler(UniversalFile):
+    def __init__(self, filename):
+        UniversalFile.__init__(self, filename)
+        try:
+            self.flat_file = open(filename, 'r')
+            self.base_file = csv.reader(self.flat_file)
+        except IOError:
+            raise FileHandlerError('File error. Please make sure you types the correct filename.')
+
+    def next(self):
+        try:
+            return self.base_file.next()
+        except StopIteration:
+            self.flat_file.seek(0)
+            raise StopIteration
+
+class ExcelHandler(UniversalFile):
+    def __init__(self, filename, header_row = None):
+        UniversalFile.__init__(self, filename)
+        try:
+            self.data_frame = pd.read_excel(filename, header=header_row)
+            cols = sheet.columns.values
+            self.base_file = (tuple(x) for x in subset.values)
+        except IOError:
+            raise FileHandlerError('File error. Please make sure you types the correct filename.')
+
+    def next(self):
+        try:
+            return self.base_file.next()
+        except StopIteration:
+            self.base_file = (tuple(x) for x in subset.values)
+            raise StopIteration
+
+# class FileHandler()
 
 
-	def open_file(self):
-		extension = self.filename.split('.')[-1]
-		if self.extension in self.extension_list:
-
+if __name__ == '__main__':
+    argparser = ArgumentParser()
+    argparser.add_argument("input", type=str, help="File to input.")
+    args = argparser.parse_args()
+    f = CSVHandler(args.input)
+    for line in f:
+        print line
